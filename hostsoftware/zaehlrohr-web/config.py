070b86045a4d67417046dbb1a0aa1141485bbc5b
LongHairedHacker <sebastian@sebastians-site.de>
2015-08-08 00:58:40 +0200

    Added per event themes Added new theme for CCCAMP15

--- hostsoftware/zaehlrohr-web/config.py
+++ hostsoftware/zaehlrohr-web/config.py
@@ -1,9 +1,10 @@
 #!/bin/env python2
 
-CURRENT_EVENT = '31C3'
+CURRENT_EVENT = 'CCCAMP15'
 DEFAULT_NODE = 'CentralNode'
 
 NODES = {
 	'30C3': ['Cbase', 'Baellebad', 'ErsterStock'],
 	'31C3': ['CentralNode', 'HardwareHackingArea', 'SoftwareAndOpenSource', 'Comix' 'Milliways','Netze', 'BallPit'],
+	'CCCAMP15': ['CentralNode']
 }
