0d7ffbd6f3b7990dab5244ade064e21350b803b3
LongHairedHacker <sebastian@sebastians-site.de>
2015-12-18 14:37:42 +0100

    First design for 32C3 New Zaehlrohr logo

--- hostsoftware/zaehlrohr-web/config.py
+++ hostsoftware/zaehlrohr-web/config.py
@@ -6,5 +6,6 @@ DEFAULT_NODE = 'CentralNode'
 NODES = {
 	'30C3': ['Cbase', 'Baellebad', 'ErsterStock'],
 	'31C3': ['CentralNode', 'HardwareHackingArea', 'SoftwareAndOpenSource', 'Comix' 'Milliways','Netze', 'BallPit'],
-	'CCCAMP15': ['CentralNode', 'FoodHackingVillage', 'metalab', 'Roadside', 'Milliways']
+	'CCCAMP15': ['CentralNode', 'FoodHackingVillage', 'metalab', 'Roadside', 'Milliways'],
+	'32C3' : ['CentralNode', 'HardwareHackingArea', 'SoftwareAndOpenSource', 'Comix' 'Milliways','Netze', 'BallPit'],
 }
