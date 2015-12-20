d7955f06e8d7bd1c2c7daf0440a43f6388be4442
LongHairedHacker <sebastian@sebastians-site.de>
2015-12-20 02:03:19 +0100

    Rewrote import tool for new json format

--- hostsoftware/common/psql/importold.py
+++ hostsoftware/common/psql/importold.py
@@ -6,22 +6,25 @@ from datetime import datetime
 from dbmanager import DBManager
 
 def print_event(event):
-	print "%s -> %s %s %s" % (event['start'], event['end'], event['velocity'], str(datetime.fromtimestamp(event['time'])))
+	print "%s -> %s %s %s" % (event['origin'], event['destination'], event['velocity'], str(datetime.fromtimestamp(event['time'])))
 
+OLD_PATH = "./old_data"
 
-dbman = DBManager("dbname=zaehlrohr user=zaehlrohr", "30C3")
-
-
-for subdir, _, files in os.walk("./old_data"):
-	for filename in files:
-		filepath = os.path.join(subdir, filename)
+for filename in os.listdir(OLD_PATH):
+	filepath = os.path.join(OLD_PATH, filename)
+	if os.path.isfile(filepath):
+		dbman = None
 		print "Reading file %s" % filepath
 		json_file = open(filepath)
 		for event in json.load(json_file):
 			print_event(event)
+
+			if dbman == None:
+				dbman = DBManager("dbname=zaehlrohr user=zaehlrohr", event['event'])
+
 			new_event = {
-				'origin' : event['start'], 
-				'destination' : event['end'], 
+				'origin' : event['origin'],
+				'destination' : event['destination'],
 				'velocity' : event['velocity'],
 				'time' : event['time']}
 			dbman.insert_event(new_event)
